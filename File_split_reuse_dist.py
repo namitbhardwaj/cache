69d2498832aa5a979d8648bdd367e28f3d87b5f0
Namit <nbhardwaj@cares2.iitdh.ac.in>
2020-05-20 14:26:59 +0530

    shift to colab

--- File_split_reuse_dist.py
+++ File_split_reuse_dist.py
@@ -13,16 +13,21 @@ import matplotlib.pyplot as plt
 from tabulate import tabulate
 
 
-# In[ ]:
+# In[1]:
 
 
 # ADDR = './401part.csv'
 
 # file name below also in os delete command
-
-ADDR = '/nfs_home/SPEC_2006/400.perlbench/400.perlbench.pinatrace.csv'
-final_ADDR = '/nfs_home/nbhardwaj/data/set_data/400'
-
+ADDR_LIST = ['/nfs_home/matkade/pinplay/Pintool/SPEC_2017_ST_First_run/510.parest_r/510.parest_r.pinatrace.csv',
+#             '/nfs_home/matkade/pinplay/Pintool/SPEC_2017_ST_First_run/511.povray_r/511.povray_r.pinatrace.csv',
+#              '/nfs_home/matkade/pinplay/Pintool/SPEC_2017_ST_First_run/526.blender_r/526.blender_r.pinatrace.csv',
+#              '/nfs_home/matkade/pinplay/Pintool/SPEC_2017_ST_First_run/600.perlbench_s/600.perlbench_s.pinatrace.csv',
+#              '/nfs_home/matkade/pinplay/Pintool/SPEC_2017_ST_First_run/602.gcc_s/602.gcc_s.pinatrace.csv',
+            ]
+# ADDR = '/nfs_home/SPEC_2006/400.perlbench/400.perlbench.pinatrace.csv'
+final_ADDR = '/nfs_home/nbhardwaj/data/SPEC2017/'
+addr_l = [510, 511, 526, 600, 602]
 # df = pd.read_csv(ADDR, skiprows = [0], nrows = 10000)
 
 
@@ -36,53 +41,55 @@ final_ADDR = '/nfs_home/nbhardwaj/data/set_data/400'
 
 
 iter = 0
-for df in pd.read_csv(ADDR, skiprows = [0], chunksize = 100000000):
-    iter += 1
-    print("---------iters-------------->", iter)
+for iaddr, ADDR in enumerate(ADDR_LIST):
+    print("---------processing list----->", ADDR)
+    for df in pd.read_csv(ADDR, skiprows = [0], usecols = ['ICount', 'Instruction', 'Mode', 'Data'],chunksize = 100000000):
+        iter += 1
+        print("---------iters-------------->", iter)
+
+        df.rename(columns = {'Data':'hex_Data', 'Instruction':'hex_Instruction'}, inplace = True) 
+
+    #     df.drop('ICount', axis = 1, inplace = True)
+
+        print("No of lines before->",len(df))
+        df = df.dropna()
+        print("No of lines after dropping nan",len(df))
+
+        #Pre-processing
+        # HEX to INT
+        df['Instruction'] = df.hex_Instruction.apply(lambda x:int(x,16))
+
+
+        # Change to line No resolution, remove the offset data
+        df['Data'] = df.hex_Data.apply(lambda x:int(x, 16)//64)
+        print("done hex to int")
+        # df['new_Data'] = df.Data.apply(lambda x: x//64)
+
+
+        # HEX to Binary with pre-padding till 64 spaces
+        df['set'] = df.hex_Instruction.apply(lambda x: int((bin(int(x, 16))[2:].zfill(64))[-12:-6], 2))
+        # df.reset_index(inplace = True, level = 'orig_idx')
+
+        # Least significant 7 to 12 bits represent the set
+        # df['set'] = df.bin_Data.apply(lambda x: int(x[-12:-6], 2))
+        print("done preprocessing")
+
+        # Grouping values with the same set no
+        df.sort_values(by = 'set', inplace = True, kind = 'mergesort')
+        print("done sorting")
+
+        for curr_set in df['set'].unique():
+            faddr = final_ADDR+str(addr_l[iaddr])+'_'+str(curr_set)+'.csv'
+            print("Processing for set->", curr_set)
+            df_sub = df[df.set.eq(curr_set)][['ICount','Instruction','Data', 'Mode','set']]
     
-    df.rename(columns = {'Data':'hex_Data', 'Instruction':'hex_Instruction'}, inplace = True) 
-
-#     df.drop('ICount', axis = 1, inplace = True)
-
-    print("No of lines before->",len(df))
-    df = df.dropna()
-    print("No of lines after dropping nan",len(df))
-
-    #Pre-processing
-    # HEX to INT
-    df['Instruction'] = df.hex_Instruction.apply(lambda x:int(x,16))
-
-
-    # Change to line No resolution, remove the offset data
-    df['Data'] = df.hex_Data.apply(lambda x:int(x, 16)//64)
-    print("done hex to int")
-    # df['new_Data'] = df.Data.apply(lambda x: x//64)
-
-
-    # HEX to Binary with pre-padding till 64 spaces
-    df['set'] = df.hex_Instruction.apply(lambda x: int((bin(int(x, 16))[2:].zfill(64))[-12:-6], 2))
-    # df.reset_index(inplace = True, level = 'orig_idx')
-
-    # Least significant 7 to 12 bits represent the set
-    # df['set'] = df.bin_Data.apply(lambda x: int(x[-12:-6], 2))
-    print("done preprocessing")
-
-    # Grouping values with the same set no
-    df.sort_values(by = 'set', inplace = True, kind = 'mergesort')
-    print("done sorting")
-
-
-    for curr_set in df['set'].unique():
-        print("Processing for set->", curr_set)
-        df_sub = df[df.set.eq(curr_set)][['ICount','Instruction','Data', 'Mode','set']]
-
-        # to allow chunk processing
-        if not os.path.isfile(final_ADDR+'_'+str(curr_set)+'.csv'):
-            df_sub.to_csv(final_ADDR+'_'+str(curr_set)+'.csv', index = False)
-        else:
-            df_sub.to_csv(final_ADDR+'_'+str(curr_set)+'.csv', mode = 'a', header = False, index = False)
+            # to allow chunk processing
+            if not os.path.isfile(faddr):
+                df_sub.to_csv(faddr, index = False)
+            else:
+                df_sub.to_csv(faddr, mode = 'a', header = False, index = False)
+        #     break
     #     break
-#     break
 
 
 # In[ ]:
