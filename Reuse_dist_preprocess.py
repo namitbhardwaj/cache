0ccb602b245ae562094bdd810d0a915e129c99a1
Namit <nbhardwaj@cares2.iitdh.ac.in>
2020-05-23 15:44:23 +0530

    SPEC2017

--- Reuse_dist_preprocess.py
+++ Reuse_dist_preprocess.py
@@ -11,14 +11,17 @@ import sys
 import os
 import matplotlib.pyplot as plt
 from tabulate import tabulate
+import threading
 
 
 # In[48]:
 
 
-ADDR = '/nfs_home/nbhardwaj/data/set_data/400_'
-ORIG_ADDR = '/nfs_home/SPEC_2006/400.perlbench/400.perlbench.pinatrace.csv'
-final_ADDR = '/nfs_home/nbhardwaj/data/rds_data/400/'
+ADDR = '/nfs_home/nbhardwaj/data/SPEC2017/'
+addr_l = [
+#     510, 511, 526, 600, 602, 605, 
+          620, 623, 625, 631, 641, 648, 657]
+final_ADDR = '/nfs_home/nbhardwaj/data/rds_data/SPEC2017/'
 
 
 # ### Testing integrity of split files
@@ -52,29 +55,29 @@ final_ADDR = '/nfs_home/nbhardwaj/data/rds_data/400/'
 
 def create_label(x):
     if(x<=1):
-        return 1
+        return 0
     elif(x>1 and x<=2):
-        return 2
+        return 1
     elif(x>2 and x<=4):
-        return 3
+        return 2
     elif(x>4 and x<=8):
-        return 4
+        return 3
     elif(x>8 and x<=16):
-        return 5
+        return 4
     elif(x>16 and x<=32):
-        return 6
+        return 5
     elif(x>32 and x<=64):
-        return 7
+        return 6
     elif(x>64):
-        return 8
+        return 7
 
 
-# In[65]:
+# In[ ]:
 
 
-sets = [x for x in range(64)]
-for cset in sets:
-    df = pd.read_csv(ADDR + str(cset)+'.csv')
+def preprocess(ADDR, curr_filename, cset, final_ADDR):
+    print("----starting---||", curr_filename, "||",cset)
+    df = pd.read_csv(ADDR +str(curr_filename)+'_'+ str(cset)+'.csv')
     m = defaultdict(list)
         # Map : <Data val> : <indexes where it appears>
     for ind, x in enumerate(df.Data.values):
@@ -88,14 +91,30 @@ for cset in sets:
     df.Data = df.Data.astype('int64')
     df['delta'] = np.diff(df.Data.values, prepend = np.nan)
     df['label'] = df.rd.map(create_label)
-    df.to_csv(final_ADDR+str(cset)+'.csv')
-    print("------Done processing set->", cset)
+    df.to_csv(final_ADDR+str(curr_filename)+'_'+str(cset)+'.csv')
+    print("------Done processing ->", curr_filename, " ", cset)
+
+
+# In[65]:
+
+
+sets = [x for x in range(64)]
+for curr_filename in addr_l:
+#     threads = []
+    for cset in sets:
+#         t = threading.Thread(target = preprocess, args = (ADDR, curr_filename, cset, final_ADDR))
+#         threads.append(t)
+#         t.start()
+#     for t in threads:
+#         t.join()
+        preprocess(ADDR, curr_filename, cset, final_ADDR)
+    print("----finished processing---->", curr_filename)
 
 
 # In[68]:
 
 
-
+print("xxxxxxxxxxxxxx------its done")
 
 
 # In[ ]:
